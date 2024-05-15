d8ec37aac2af17779c695c4a946e3bb94b3587b4
Antonio Contreras <acontrecar@gmail.com>
2024-05-15 23:45:08 +0200

    AxiosAdapter

---
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export const DetailsScren = () => {
+  return (
+    <View>
+      <Text>DetailsScren</Text>
+    </View>
+  );
+};
